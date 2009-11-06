87475f556119ac9e89abb43dcd8064e933dea852
Kevin J. Qiu <kevin.jing.qiu@gmail.com>
2009-11-05 22:54:07 -0500

    project setup. euclidean distance score

---
+++ src/main/clojure/pci/pci.clj
@@ -0,0 +1,28 @@
+(ns pci.pci
+	(:use clojure.set pci.recommendations))
+
+(defn shared-items
+	[prefs person1 person2]
+	(intersection
+ 		(set (keys (get prefs person1)))
+		(set (keys (get prefs person2)))))
+
+(defn sum-of-squares
+	[prefs shared-items person1 person2]
+	(apply + 
+		(for [item shared-items] 
+			(Math/pow (- (get (get prefs person1) item) (get (get prefs person2) item))	2))))
+
+(defn sim-distance
+	"Returns a distance-based similarity score for person1 and person2"
+	[prefs person1 person2]
+	(let [si (shared-items prefs person1 person2)]
+		(if (empty? si) 
+			0 
+			(let [sos (sum-of-squares prefs si person1 person2)]
+				(/ 1 (+ 1 (Math/sqrt sos)))))))
+
+(defn main
+	[]
+	(sim-distance critics "Lisa Rose" "Gene Seymour"))
+	
---
+++ src/main/clojure/pci/recommendations.clj
@@ -0,0 +1,53 @@
+(ns pci.recommendations)
+
+(def critics 
+  (hash-map
+    "Lisa Rose" 
+    (hash-map
+      "Lady in the Water" 2.5
+      "Snakes on a Plane" 3.5
+      "Just My Luck" 3.0
+      "Superman Returns" 3.5
+      "You, Me and Dupree" 2.5
+      "The Night Listener" 3.0)
+    "Gene Seymour" 
+    (hash-map
+      "Lady in the Water" 3.0
+      "Snakes on a Plane" 3.5
+      "Just My Luck" 1.5
+      "Superman Returns" 5.0
+      "You, Me and Dupree" 3.0
+      "The Night Listener" 3.5)
+    "Michael Phillips"
+    (hash-map
+      "Lady in the Water" 2.5
+      "Snakes on a Plane" 3.0
+      "Superman Returns" 3.5
+      "The Night Listener" 4.0)
+    "Claudia Puig"
+    (hash-map
+      "Snakes on a Plane" 3.5
+      "Just My Luck" 3.0
+      "Superman Returns" 4.0
+      "You, Me and Dupree" 2.5
+      "The Night Listener" 4.5)
+    "Mick LaSalle"
+    (hash-map
+      "Lady in the Water" 3.0
+      "Snakes on a Plane" 4.0
+      "Just My Luck" 2.0
+      "Superman Returns" 3.0
+      "You, Me and Dupree" 2.0
+      "The Night Listener" 3.0)
+    "Jack Matthews"
+    (hash-map
+      "Lady in the Water" 3.0
+      "Snakes on a Plane" 4.0
+      "Superman Returns" 5.0
+      "You, Me and Dupree" 3.5
+      "The Night Listener" 3.0)
+    "Toby"
+    (hash-map
+      "Snakes on a Plane" 4.5
+      "You, Me and Dupree" 1.0
+      "The Night Listener" 4.0)))
