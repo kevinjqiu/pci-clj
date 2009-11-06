b812b1541852bab4b615678817b8f4401f949f3a
Kevin J. Qiu <kevin.jing.qiu@gmail.com>
2009-11-06 13:26:18 -0500

    added pearson

--- src/main/clojure/pci/pci.clj
+++ src/main/clojure/pci/pci.clj
@@ -1,44 +1,11 @@
 (ns pci.pci
-	(:use clojure.set pci.recommendations))
+    (:use clojure.set pci.recommendations))
 
-(defn key-set
-	"Return the keyset of a map"
-	[param]
-	(set (keys param)))
-
-(defn shared-items
-	[prefs person1 person2]
-	(intersection
- 		(key-set (get prefs person1))
-		(key-set (get prefs person2))))
-
-(defn sum-of-squares
-	[prefs shared-items person1 person2]
-	(apply + 
-		(for [item shared-items] 
-			(Math/pow (- (get (get prefs person1) item) (get (get prefs person2) item))	2))))
-
-(defn sim-distance
-	"Returns a distance-based similarity score for person1 and person2"
-	[prefs person1 person2]
-	(let [si (shared-items prefs person1 person2)]
-		(if (empty? si) 
-			0 
-			(let [sos (sum-of-squares prefs si person1 person2)]
-				(/ 1 (+ 1 (Math/sqrt sos)))))))
-
-(defstruct similarity :person1 :person2 :score)
 
 (defn main
-	[]
-	(let [similarities (sort-by #(:score %) (for [person1 (key-set critics) person2 (disj (key-set critics) person1)] 
-											(struct-map similarity :person1 person1 :person2 person2 :score (sim-distance critics person1 person2))))]
-		(doseq [item similarities]
-			(println item))))
-			
-		
-		
-		
-		
+    []
+    (let [similarities (sort-by #(:score %) (for [person1 (key-set critics) person2 (disj (key-set critics) person1)]
+                                            (struct-map similarity :person1 person1 :person2 person2 :score (sim-pearson critics person1 person2))))]
+        (doseq [item similarities]
+            (println item))))
 
-	
--- src/main/clojure/pci/recommendations.clj
+++ src/main/clojure/pci/recommendations.clj
@@ -1,8 +1,9 @@
-(ns pci.recommendations)
+(ns pci.recommendations
+  (:use clojure.set))
 
-(def critics 
+(def critics
   (hash-map
-    "Lisa Rose" 
+    "Lisa Rose"
     (hash-map
       "Lady in the Water" 2.5
       "Snakes on a Plane" 3.5
@@ -10,7 +11,7 @@
       "Superman Returns" 3.5
       "You, Me and Dupree" 2.5
       "The Night Listener" 3.0)
-    "Gene Seymour" 
+    "Gene Seymour"
     (hash-map
       "Lady in the Water" 3.0
       "Snakes on a Plane" 3.5
@@ -51,3 +52,52 @@
       "Snakes on a Plane" 4.5
       "You, Me and Dupree" 1.0
       "The Night Listener" 4.0)))
+
+
+(defstruct similarity :person1 :person2 :score)
+
+(defn- key-set
+  "Return the keyset of a map"
+  [param]
+  (set (keys param)))
+
+(defn- get-score
+  "Return the score of the specified item for the specified person"
+  [prefs person item]
+  (get (get prefs person) item))
+
+(defn- shared-items
+  [prefs person1 person2]
+  (intersection
+    (key-set (get prefs person1))
+    (key-set (get prefs person2))))
+
+(defn- sum-of-squares
+  [prefs shared-items person1 person2]
+  (apply +
+    (for [item shared-items]
+      (Math/pow (- (get-score prefs person1 item) (get-score prefs person2 item)) 2))))
+
+(defn sim-distance
+  "Returns a distance-based similarity score for p1 and p2"
+  [prefs p1 p2]
+  (let [si (shared-items prefs p1 p2)]
+    (if (empty? si)
+      0
+      (let [sos (sum-of-squares prefs si p1 p2)]
+        (/ 1 (+ 1 sos))))))
+
+(defn sim-pearson
+  "Returns the Pearson correlation coefficient for p1 and p2"
+  [prefs p1 p2]
+  (let [si (shared-items prefs p1 p2)
+        size (count si)
+        sum #(apply + (for [item si] (get-score prefs % item)))
+        sum-sq #(apply + (for [item si] (Math/pow (get-score prefs % item) 2)))
+        pSum (apply + (for [item si] (* (get-score prefs p1 item) (get-score prefs p2 item))))
+        n (- pSum (/ (* (sum p1) (sum p2)) size))
+        den #(- %1 (/ (Math/pow %2 2) size))]
+
+    (if (empty? si)
+      0
+      (/ n (Math/sqrt (* (den (sum-sq p1) (sum p1)) (den (sum-sq p2) (sum p2))))))))
