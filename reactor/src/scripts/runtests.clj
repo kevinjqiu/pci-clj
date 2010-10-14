ad7958eb2511f254f7598e57a27c08e1c9c5071f
Kevin J. Qiu <kevin@kevin.(none)>
2010-10-14 10:56:03 -0400

    modified .gitignore and deleted unwanted src

--- reactor/src/scripts/runtests.clj
+++
@@ -1,9 +0,0 @@
-(ns runtests
-  ;; Uncomment for Clojure 1.0 & Contrib snapshot:
-  ;; (:use clojure.contrib.test-is)
-
-  ;; Uncomment for Clojure 1.1+
-  ;; (:use clojure.test)
-)
-
-;; (run-all-tests)
