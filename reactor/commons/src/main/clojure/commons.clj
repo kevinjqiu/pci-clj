181e1122ac51813d690afbd460cea0e10b2abe07
Kevin J. Qiu <kevin.jing.qiu@gmail.com>
2010-10-14 23:51:29 -0400

    removed unwanted file. added one sample test case

--- reactor/commons/src/main/clojure/commons.clj
+++
@@ -1,8 +0,0 @@
-(ns pci.commons)
-
-(defmacro with-private-fns [[ns fns] & tests]
-  "refers private fns from ns and runs tests in context.
-   courtesy of chouser"
-  `(let ~(reduce #(conj %1 %2 `(ns-resolve '~ns '~%2)) [] fns)
-     ~@tests))
-
