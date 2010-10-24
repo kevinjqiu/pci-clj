41a03bc3e022f95da92f7df5667e54220788bdcb
Kevin J. Qiu <kevin.jing.qiu@gmail.com>
2010-10-24 02:08:55 -0400

    start gini impurity

--- test/pci_clj/test/core.clj
+++ test/pci_clj/test/core.clj
@@ -2,5 +2,96 @@
   (:use [pci-clj.core] :reload)
   (:use [clojure.test]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+(deftest bisect-smoke
+  (let [result (bisect [1 2 3 4 5 6] #(< % 3))]
+      (is 
+	        (= [1 2] (first result)) 
+			      (= [3 4 5 6] (second result)))))
+
+(deftest bisect-unordered-int
+  (let [result (bisect [5 3 6 1 2 4] #(< % 3))]
+      (is
+	        (= [1 2] (first result))
+			      (= [5 3 6 4] (second result)))))
+
+(deftest bisect-unordered-int-unmatched
+  (let [result (bisect [5 3 6 1 2 4] #(< % -3))]
+      (is
+	        (= [] (first result))
+			      (= [5 3 6 1 2 4] (second result)))))
+
+(deftest bisect-unordered-str
+  (let [result (bisect ["abc" "def" "xyz" "blah"] #(= % "abc"))]
+      (is
+	        (= ["abc"] (first result))
+			      (= ["def" "xyz" "blah"] (second result)))))
+
+
+(deftest bisect-unordered-str-unmatched
+  (let [result (bisect ["abc" "def" "xyz" "blah"] #(= % "java"))]
+      (is
+	        (= [] (first result))
+			      (= ["abc" "def" "xyz" "blah"] (second result)))))
+
+(deftest bisect-smoke
+  (let [result (bisect [1 2 3 4 5 6] #(< % 3))]
+      (is 
+	        (= [1 2] (first result)) 
+			      (= [3 4 5 6] (second result)))))
+
+(deftest bisect-unordered-int
+  (let [result (bisect [5 3 6 1 2 4] #(< % 3))]
+      (is
+	        (= [1 2] (first result))
+			      (= [5 3 6 4] (second result)))))
+
+(deftest bisect-unordered-int-unmatched
+  (let [result (bisect [5 3 6 1 2 4] #(< % -3))]
+      (is
+	        (= [] (first result))
+			      (= [5 3 6 1 2 4] (second result)))))
+
+(deftest bisect-unordered-str
+  (let [result (bisect ["abc" "def" "xyz" "blah"] #(= % "abc"))]
+      (is
+	        (= ["abc"] (first result))
+			      (= ["def" "xyz" "blah"] (second result)))))
+
+
+(deftest bisect-unordered-str-unmatched
+  (let [result (bisect ["abc" "def" "xyz" "blah"] #(= % "java"))]
+      (is
+	        (= [] (first result))
+			      (= ["abc" "def" "xyz" "blah"] (second result)))))
+
+(deftest bisect-smoke
+  (let [result (bisect [1 2 3 4 5 6] #(< % 3))]
+      (is 
+	        (= [1 2] (first result)) 
+			      (= [3 4 5 6] (second result)))))
+
+(deftest bisect-unordered-int
+  (let [result (bisect [5 3 6 1 2 4] #(< % 3))]
+      (is
+	        (= [1 2] (first result))
+			      (= [5 3 6 4] (second result)))))
+
+(deftest bisect-unordered-int-unmatched
+  (let [result (bisect [5 3 6 1 2 4] #(< % -3))]
+      (is
+	        (= [] (first result))
+			      (= [5 3 6 1 2 4] (second result)))))
+
+(deftest bisect-unordered-str
+  (let [result (bisect ["abc" "def" "xyz" "blah"] #(= % "abc"))]
+      (is
+	        (= ["abc"] (first result))
+			      (= ["def" "xyz" "blah"] (second result)))))
+
+
+(deftest bisect-unordered-str-unmatched
+  (let [result (bisect ["abc" "def" "xyz" "blah"] #(= % "java"))]
+      (is
+	        (= [] (first result))
+			      (= ["abc" "def" "xyz" "blah"] (second result)))))
+
