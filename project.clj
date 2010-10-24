4b14e990bf392eb1b7531217ca0883225dbcf1cd
Kevin J. Qiu <kevin.jing.qiu@gmail.com>
2010-10-23 23:10:43 -0400

    aadded description

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject pci-clj "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
+  :description "Programming Collective Intelligence"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]])
